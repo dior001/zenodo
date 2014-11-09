168fbc73c7fdee02121f62b98f9237b39b497e83
David Iorns <david.iorns@gmail.com>
2014-11-09 09:59:31 -0800

    Improve depositions spec

--- spec/zenodo/dsl/depositions_spec.rb
+++ spec/zenodo/dsl/depositions_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe Zenodo::DSL::Depositions do
-  let(:deposition_id) do
-    deposition = Zenodo.client.create_deposition(deposition: {})
-    deposition.id
+  let!(:deposition_id) do
+    VCR.use_cassette('create_deposition') do
+      deposition = Zenodo.client.create_deposition(deposition: {})
+      deposition.id
+    end
+  end
+  let!(:deleted_deposition_id) do
+    VCR.use_cassette('create_deposition_for_deletion') do
+      deposition = Zenodo.client.create_deposition(deposition: {})
+      deposition.id
+    end
   end
 
   # GET /Deposit/Depositions
@@ -18,42 +26,42 @@ describe Zenodo::DSL::Depositions do
   end
   
   # GET /Deposit/Depositions/{id}
-  # describe '#get_deposition' do
-  #   it 'returns a deposition' do
-  #     VCR.use_cassette('get_deposition') do
-  #       expect(Zenodo.client.get_deposition(id: deposition_id)).to be_a(Deposition)
-  #     end
-  #   end
-  # end
+  describe '#get_deposition' do
+    it 'returns a deposition' do
+      VCR.use_cassette('get_deposition') do
+        expect(Zenodo.client.get_deposition(id: deposition_id)).to be_a(Deposition)
+      end
+    end
+  end
 
   # POST /Deposit/Depositions
-  # describe '#create_deposition' do
-  #   it 'creates a deposition' do
-  #     VCR.use_cassette('create_deposition') do
-  #       response = Zenodo.client.create_deposition(deposition: {})
-  #       expect(response).to be_a(Deposition)
-  #     end
-  #   end
-  # end
+  describe '#create_deposition' do
+    it 'creates a deposition' do
+      VCR.use_cassette('create_deposition') do
+        response = Zenodo.client.create_deposition(deposition: {})
+        expect(response).to be_a(Deposition)
+      end
+    end
+  end
 
   # PUT /Deposit/Depositions
-  # describe '#update_deposition' do
-  #   it 'updates a deposition' do
-  #     VCR.use_cassette('update_deposition') do
-  #       deposition = Zenodo.client.get_deposition(id: deposition_id)
-  #       response = Zenodo.client.update_deposition(deposition: deposition)
-  #       expect(response).to be_a(Deposition)
-  #     end
-  #   end
-  # end
+  describe '#update_deposition' do
+    it 'updates a deposition' do
+      VCR.use_cassette('update_deposition') do
+        deposition = Zenodo.client.get_deposition(id: deposition_id)
+        response = Zenodo.client.update_deposition(deposition: deposition)
+        expect(response).to be_a(Deposition)
+      end
+    end
+  end
 
   # DELETE /Deposit/Depositions/{id}
-  # describe '#delete_deposition' do
-  #   it 'returns a response with code 202' do
-  #     VCR.use_cassette('delete_deposition') do
-  #       response = Zenodo.client.delete_deposition(id: deposition_id)
-  #       expect(response.status).to eq(202)
-  #     end
-  #   end
-  # end
+  describe '#delete_deposition' do
+    it 'returns a response with code 204' do
+      VCR.use_cassette('delete_deposition') do
+        response = Zenodo.client.delete_deposition(id: deleted_deposition_id)
+        expect(response.status).to eq(204)
+      end
+    end
+  end
 end
