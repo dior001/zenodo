0f307d18c8e3a191e6540be94c70d82f451f3372
David Iorns <david.iorns@gmail.com>
2014-11-09 12:34:00 -0800

    Add publish action

--- lib/zenodo/dsl/deposition_actions.rb
+++ lib/zenodo/dsl/deposition_actions.rb
@@ -3,6 +3,14 @@ require 'zenodo/dsl'
 module Zenodo
   module DSL::DepositionActions
     # Publish POST deposit/depositions/:id/actions/publish
+    # Publishes a deposition.
+    # @param [String, Fixnum] id A deposition's ID.
+    # @raise [ArgumentError] If the method arguments are blank.
+    # @return [Zenodo::Resources::deposition, nil].
+    def publish_deposition(id:)
+      raise ArgumentError, "ID cannot be blank" if id.blank?
+      Resources::Deposition.parse(request(:post, "deposit/depositions/#{id}/actions/publish"))
+    end
 
     # Edit POST deposit/depositions/:id/actions/edit
 
--- spec/zenodo/dsl/deposition_actions_spec.rb
+++ spec/zenodo/dsl/deposition_actions_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Zenodo::DSL::DepositionActions do
+  let(:deposition_attributes) do
+    {
+      'metadata' => {
+        'title' => 'My first upload',
+        'upload_type' => 'poster',
+        'description' => 'This is my first upload',
+        'creators' =>[{'name' => 'Doe, Jane','affiliation' => 'ZENODO'}]
+      }
+    }
+  end
+  let!(:deposition_id) do
+    VCR.use_cassette('create_deposition_for_publishing') do
+      deposition = Zenodo.client.create_deposition(deposition: deposition_attributes)
+      deposition['id']
+    end
+  end
 
+  # Publish POST deposit/depositions/:id/actions/publish
+  describe '#publish_deposition' do
+    it 'publishes a deposition' do
+      VCR.use_cassette('publish_deposition') do
+        response = Zenodo.client.publish_deposition(id: deposition_id)
+        expect(response).to be_a(Deposition)
+      end
+    end
+  end
+
+  # Edit POST deposit/depositions/:id/actions/edit
+
+  # Discard POST deposit/depositions/:id/actions/discard
 end
